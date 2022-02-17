04a77b5fac70c0ced94ed8a696a141971c016a5c
deborahtalita <72002059+deborahtalita@users.noreply.github.com>
2022-02-17 07:25:40 +0700

    update

--- web-profile/strapi-react/src/component/MemberCard.js
+++ web-profile/strapi-react/src/component/MemberCard.js
@@ -1,12 +1,12 @@
 export default function MemberCard({data}){
     return (
         <div className="card mb-5">
-            <img src="https://i.pinimg.com/564x/6d/69/d4/6d69d40d8feb76f70914d2dab645171a.jpg"
+            <img src={"https://i.pinimg.com/564x/6d/69/d4/6d69d40d8feb76f70914d2dab645171a.jpg"}
             className="card-img-top" alt="..."/>
         <div className="card-body">
             <h6 className="card-title">{data.attributes.Name}</h6>
             <p className="card-text">{data.attributes.NIM}</p>
-            
+            <p className="card-role fw-bold">{data.attributes.job.data.attributes.jobname}</p>
             <a href="#" className="btn btn-primary" style={{backgroundColor: '#4caf50'}}>Details</a>
         </div>
         </div>
--- web-profile/strapi-react/src/config/MemberAPI.js
+++ web-profile/strapi-react/src/config/MemberAPI.js
@@ -1,6 +1,6 @@
 import api from ".";
 
 export default {
-    find: () => api.get("/api/members").then((res) => res.data),
+    find: () => api.get("/api/members?populate=*").then((res) => res.data),
     detail: (id) => api.get(`/api/members/${id}`).then((res) => res.data),
 }
