7fa1729bb216ee28160171be63e57eb6d83d87ca
okybagus871 <oky.bagus.tif20@polban.ac.id>
2022-02-17 12:19:55 +0700

    fe

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import DataMahasiswa from './components/DataMahasiswa';
+
+function App() {
+	return (
+		<div className="App">
+			<Header />
+			<DataMahasiswa />
+		</div>
+	);
+}
+
+export default App;
