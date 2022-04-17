670e8e1adc7f85573857bb900789603b953bce7f
okybagus871 <91017338+okybagus871@users.noreply.github.com>
2022-04-17 16:05:48 +0700

    crud data penguji

--- frontend/PLN/app/Http/Controllers/Master/DataPengujiController.php
+++ frontend/PLN/app/Http/Controllers/Master/DataPengujiController.php
@@ -4,10 +4,55 @@ namespace App\Http\Controllers\Master;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Crypt;
 
 class DataPengujiController extends Controller
 {
     public function index(){
-        return view('page.master.dataPenguji');
+        $penguji = Http::get('http://localhost:1337/api/pegawais?populate=jabatan');
+        return view('page.master.dataPenguji', [
+            "penguji" => json_decode($penguji)
+        ]);
+    }
+
+    public function add()
+    {
+        $penguji = Http::get('http://localhost:1337/api/pegawais?populate=jabatan');
+        return view('page.master.addDataPenguji', [
+            "penguji" => json_decode($penguji)
+        ]);
+    }
+
+    public function getPengujiById($id)
+    {
+        $decryptId = Crypt::decryptString($id);
+        $penguji = Http::get('http://localhost:1337/api/pegawais/'.$decryptId);
+        return view('page.master.editDataPenguji', [
+            "penguji" => json_decode($penguji)
+        ]);
+    }
+
+    public function updateData(Request $request)
+    {
+        $color = "teal";
+        $msg = "Berhasil mengubah data penguji";
+        $decryptId = Crypt::decryptString($request->id);
+        $nama = $request->nama;
+        Http::put('http://localhost:1337/api/pegawais/'.$decryptId, [
+            'data' => [
+                'namaPegawai' => $nama
+            ]
+        ]);
+        return redirect()->route('getPenguji')->with(compact('msg', 'color'));
+    }
+
+    public function delete($id)
+    {
+        $color = "teal";
+        $msg = "Berhasil menghapus data penguji";
+        $decryptId = Crypt::decryptString($id);
+        Http::delete('http://localhost:1337/api/pegawais/'.$decryptId);
+        return redirect()->route('getPenguji')->with(compact('msg', 'color'));
     }
 }
--- frontend/PLN/routes/web.php
+++ frontend/PLN/routes/web.php
@@ -18,11 +18,16 @@ Route::get('dashboard', 'DashboardController@index')->name('getDashboard');
 
 Route::group(['prefix' => 'master'], function () {
     Route::get('dataPeserta', 'Master\DataPesertaController@index')->name('getPeserta');
-    Route::get('dataPenguji', 'Master\DataPengujiController@index')->name('getPenguji');
     Route::get('dataPeserta/json', 'Master\DataPesertaController@json')->name('getJsonPeserta');
     Route::get('dataPeserta/add', 'Master\DataPesertaController@add')->name('dataPeserta.add');
     Route::get('dataPeserta/updateRole/{id}', 'Master\DataPesertaController@updateRole')->name('dataPeserta.updateRole');
     Route::get('dataPeserta/getPesertaById/{id}', 'Master\DataPesertaController@getPesertaById')->name('dataPeserta.getPesertaById');
     Route::post('dataPeserta/updateData', 'Master\DataPesertaController@updateData')->name('dataPeserta.updateData');
     Route::get('dataPeserta/delete/{id}', 'Master\DataPesertaController@delete')->name('dataPeserta.delete');
+
+    Route::get('dataPenguji', 'Master\DataPengujiController@index')->name('getPenguji');
+    Route::get('dataPenguji/add', 'Master\DataPengujiController@add')->name('dataPenguji.add');
+    Route::get('dataPenguji/getPengujiById/{id}', 'Master\DataPengujiController@getPengujiById')->name('dataPenguji.getPengujiById');
+    Route::post('dataPenguji/updateData', 'Master\DataPengujiController@updateData')->name('dataPenguji.updateData');
+    Route::get('dataPenguji/delete/{id}', 'Master\DataPengujiController@delete')->name('dataPenguji.delete');
 });
